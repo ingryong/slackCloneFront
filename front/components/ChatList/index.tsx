ed509696c56308c0205f1ba4ea43bda77e47aaae
ingryong <bbgod2@gmail.com>
2023-02-13 23:10:50 +0900

    스크롤을 가장 위로 올렸을 때, 과거 챗 데이터 가져오기

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,38 +1,47 @@
-import React, { useCallback, useRef, VFC } from 'react';
+import React, { useCallback, forwardRef } from 'react';
 import Chat from '@components/Chat';
 import { IChat, IDM } from '@typings/db';
 import { ChatZone, Section, StickyHeader } from './styles';
-import { Scrollbars } from 'react-custom-scrollbars'
+import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
-  chatSectoins: {[key:string]: IDM[]};
+  chatSectoins: { [key: string]: IDM[] };
+  setSize: (f: (size: number) => number) => Promise<IDM[][] | undefined>;
+  isEmpty: boolean;
+  isReachingEnd: boolean;
 }
 
 /** Chat에서 가져온 채팅을 뿌려줌 */
-const ChatList: VFC<Props> = ({chatSectoins}) => {
-  const scrollbarRef = useRef(null);
-
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSectoins, setSize, isEmpty, isReachingEnd }, ref) => {
   // 채팅 리스트 위로 올릴 때 과거 채팅들 가져올 수 있게
-  const onScroll = useCallback(()=>{},[])
-  
+  const onScroll = useCallback((values) => {
+    // 가장 위일 때 그리고 불러올 데이터가 있을 때 데이터 추가 로딩
+    if (values.scrollTop === 0 && !isReachingEnd) {
+      console.log('가장 위');
+      setSize((prevSize) => prevSize + 1).then(() => {
+        // 스크롤 위치 유지
+      });
+    }
+  }, []);
+
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
-        {Object.entries(chatSectoins).map(([date, chats])=>{
+      <Scrollbars autoHide ref={ref} onScrollFrame={onScroll}>
+        {Object.entries(chatSectoins).map(([date, chats]) => {
           return (
             <Section className={`section-${date}`} key={date}>
               <StickyHeader>
                 <button>{date}</button>
               </StickyHeader>
-              {chats.map((chat)=>(
-                <Chat key={chat.id} data={chat}/>
+              {chats.map((chat) => (
+                <Chat key={chat.id} data={chat} />
               ))}
             </Section>
-          )
+          );
         })}
       </Scrollbars>
     </ChatZone>
   );
-};
+});
 
 export default ChatList;
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import gravatar from 'gravatar';
 import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite';
 import fetcher from '@utils/fetcher';
 import { useParams } from 'react-router';
 import ChatBox from '@components/ChatBox';
@@ -10,6 +11,7 @@ import axios from 'axios';
 import { IDM } from '@typings/db';
 import { Container, Header } from './styles';
 import makeSection from '@utils/makeSection';
+import Scrollbars from 'react-custom-scrollbars';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
@@ -18,10 +20,20 @@ const DirectMessage = () => {
   const [chat, onChangeChat, setChat] = useInput('');
 
   // 등록된 채팅 받아오기
-  const { data: chatData, mutate } = useSWR<IDM[]>(
-    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+  const {
+    data: chatData,
+    mutate,
+    setSize,
+  } = useSWRInfinite<IDM[]>(
+    (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
     fetcher,
   );
+
+  // 챗 데이터를 모두 가져와 더이상 가져올 데이터가 없을 때
+  const isEmpty = chatData?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
+
+  const scrollbarRef = useRef<Scrollbars>(null);
 
   // 작성된 내용 submit
   const onSubmitForm = useCallback(
@@ -31,7 +43,7 @@ const DirectMessage = () => {
       if (chat?.trim() && chatData) {
         console.log(chat);
         axios
-        .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
             content: chat,
           })
           .then(() => {
@@ -48,8 +60,8 @@ const DirectMessage = () => {
     return null;
   }
 
-  // 날짜별 묶음 적용
-  const chatSectoins = makeSection(chatData ? [...chatData].reverse() : []);
+  // 날짜별 묶음 적용. 2차원 배열을 flat()을 사용하여 1차원 배열로 바꿈
+  const chatSectoins = makeSection(chatData ? chatData.flat().reverse() : []);
 
   return (
     <Container>
@@ -57,7 +69,13 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatSectoins={chatSectoins} />
+      <ChatList
+        chatSectoins={chatSectoins}
+        ref={scrollbarRef}
+        setSize={setSize}
+        isEmpty={isEmpty}
+        isReachingEnd={isReachingEnd}
+      />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
